abef5790fede80eee9f738a48541158b39a6f79c
objektwerks <objektwerks@runbox.com>
2023-05-11 14:33:12 -0400

    edited test

--- src/test/scala/objektwerks/OSLibTest.scala
+++ src/test/scala/objektwerks/OSLibTest.scala
@@ -3,6 +3,11 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 
+import os.*
+
 class OSLibTest extends AnyFunSuite with Matchers:
-  test("test") {
+  val target = pwd / "target"
+
+  test("write > read") {
+    println(target)
   }
