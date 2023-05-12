cdd3e2574458a2556b099f553f1e4e7ceb327b84
objektwerks <objektwerks@runbox.com>
2023-05-12 15:19:17 -0400

    simplified ck app

--- src/main/scala/objektwerks/ChuckNorrisApp.scala
+++ src/main/scala/objektwerks/ChuckNorrisApp.scala
@@ -2,5 +2,4 @@ package objektwerks
 
 import os.*
 
-object ChuckNorrisApp:
-  @main def run(): Unit = proc("curl", "https://api.chucknorris.io/jokes/random").call(stdout = Inherit)
+@main def run(): Unit = proc("curl", "https://api.chucknorris.io/jokes/random").call(stdout = Inherit)
